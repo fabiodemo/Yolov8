988802f1e2094aec7f94928cfda1fd2275d5d211
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-08-21 15:26:22 -0300

    Inserção de arquivos e do codigo inicial

---
+++ main.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+# Load yolov8 model
+model = YOLO('yolov8n.py')
+
+# Load video
+
+
+# Read frames
+
+# Detect objects
+
+# Track objects
+
+# Plot results
+
+# Visualize
